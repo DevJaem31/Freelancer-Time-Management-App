48b6f70b209ac1b0b097e0eaa3197e05d9ae8da9
John Mark E. Flameno <177906847+DevJaem31@users.noreply.github.com>
2025-04-09 01:22:58 +0800

    Time Management App v1.0.4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MainRoutes from './routes/main-routes';
 import { Toaster } from 'react-hot-toast';
+import { apiUrl } from './services/user-services';
 
 function App() {
+	useEffect(() => {
+		apiUrl();
+	}, []);
+
 	return (
 		<div className='app-main-container'>
 			<div className='app-content-container'>
